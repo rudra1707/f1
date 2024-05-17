667ebded2bdc4f893cf0fa5479dd523e9bcd6962
rudra <bhattrudra17072005@gmail.com>
2024-05-17 14:59:02 -0400

    created function to fill flight information and did memory allocation

---
+++ f1.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_DESTINATION_LENGTH 30
+#define MAX_DATE_LENGTH 30
+#define MAX_NUMBER_OF_FLIGHTS 10
+
+// Struct to store FlightInfo
+struct FlightInfo
+{
+    char* destination;
+    char* date;
+};
+
+
+// Function to fill FlightInfo with destination and date information
+void fillFlightInfo(struct FlightInfo* flight, const char* destination, const char* date)
+{
+
+    // Memory allocation for destination
+    (*flight).destination = (char*)malloc(strlen(destination) + 1);
+    if ((*flight).destination != NULL)
+    {
+        // Copying destination string to allocated memory
+        strcpy_s((*flight).destination, strlen(destination) + 1, destination);
+    }
+
+    // Memory allocation for date
+    (*flight).date = (char*)malloc(strlen(date) + 1);
+    if ((*flight).date != NULL)
+    {
+
+        // Copying data string to allocated memory
+        strcpy_s((*flight).date, strlen(date) + 1, date);
+    }
+}
